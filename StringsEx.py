689be8bfec9e9149f2499484141d4030da075277
Tsheltrim-Dema <03230364.gcbs@rub.edu.bt>
2024-03-07 15:12:27 +0600

    excerise

---
+++ StringsEx.py
@@ -0,0 +1,7 @@
+# get user input
+# convert the user input into ALL upper case
+# print the convered string
+
+userInput = input("Enter a string: ")
+userInputConverted = userInput.upper()
+print(userInputConverted)
