d90a895a33550f6f3a439465fa208e0b2a8ed37e
Tsheltrim-Dema <03230364.gcbs@rub.edu.bt>
2024-04-26 12:18:55 +0600

    stack problam

--- unit4/b.py
+++ unit4/b.py
@@ -30,16 +30,48 @@
 #print(final_floor("(((("))
 #print(final_floor("((())))(()(())())(()())))))((()())()))()()())())(())(()(())(())()(((()()(((((()(((()))()(()(())()))(((("))  
 
-input_str = "((())))(()(())())(()())))))((()())()))()()())())(())(()(())(())()(((()()(((((()(((()))()(()(())()))(((("
+#input_str = "((())))(()(())())(()())))))((()())()))()()())())(())(()(())(())()(((()()(((((()(((()))()(()(())()))(((("
 floor_index= 0
 
 for i in input_str:
     if i== "(":
         floor_index= floor_index+1
     if i ==")":
-        floor_index= floor_index-1
+        floor_index= floor_index-1      
 print('the final floor is', floor_index)
 
 # problem 2
 # ? 
 
+def is_valid_brackets(input_str):
+    stack = []
+    brackets = {'(': ')', ')': '('}
+    for char in input_str:
+        if char in brackets:
+            if stack and brackets[char] == stack[-1]:
+                stack.pop()
+            else:
+                stack.append(char)
+    return not stack
+
+# Example usage
+input_str = "()(()"
+print(is_valid_brackets(input_str))
+
+def is_valid_brackets(input_str):
+    stack = []
+    for char in input_str:
+        if char == '(':
+            stack.append(char)
+        elif char == ')':
+            if stack and stack[-1] == '(':
+                stack.pop()
+            else:
+                return False
+    return not stack
+
+print(is_valid_brackets("()"))  
+print(is_valid_brackets("(())"))  
+print(is_valid_brackets("(()))")) 
+print(is_valid_brackets("((()))")) 
+print(is_valid_brackets(")("))
